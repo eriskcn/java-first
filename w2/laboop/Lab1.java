cdecb4fdb0dc2eab640e7de7f7ea46a2ede2c67f
eriskcn <93506716+eriskcn@users.noreply.github.com>
2024-07-23 07:07:31 +0000

    lab1 oop

---
+++ w2/laboop/Lab1.java
@@ -0,0 +1,9 @@
+package w2.laboop;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        Person hoangdz = new Person();
+        hoangdz.inputDetails();
+        hoangdz.displayDetails();
+    }
+}
---
+++ w2/laboop/Person.java
@@ -0,0 +1,39 @@
+package w2.laboop;
+
+import java.util.Scanner;
+
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    Person() {
+        this.name = "";
+        this.age = 0;
+        this.address = "";
+    }
+
+    Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public void inputDetails() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter information");
+            this.name = scanner.nextLine();
+            this.address = scanner.nextLine();
+            this.age = scanner.nextInt();
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age + " years old");
+        System.out.println("Address: " + this.address);
+    }
+}
